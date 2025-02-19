7292f33e70942a0836ca580de541e6b8266e7250
unknown <HenriqueCoruqieri>
2025-02-19 00:08:27 -0300

    feat: add db seed script

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-x1">
+      <h1 className="text-red-500">Hello World</h1>;
+    </div>
+  );
+};
+
+export default ProductPage;
