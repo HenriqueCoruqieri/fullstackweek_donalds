09c03c378034bd243e1699d984948a36905adff2
HenriqueCoruqieri <heoliveirac@gmail.com>
2025-02-25 18:05:57 -0300

    feat: created the initial screen on "localhost3000" and loading button

---
+++ src/app/[slug]/init/init-buttom.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useParams, useRouter } from "next/navigation";
+import { Loader2Icon } from "lucide-react";
+import { useState } from "react";
+
+//Client Component
+
+interface InitProps {}
+const InitButtom = ({}: InitProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const clickToLogin = useRouter();
+  const [isPending, startTransition] = useState(false);
+
+  const handleClick = async () => {
+    startTransition(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    startTransition(false);
+    clickToLogin.push(`http://localhost:3000/fsw-donalds`);
+  };
+
+  return (
+    /* Botão */
+    <div className="p-5">
+      <div className="grid items-center justify-centerd">
+        <div className="flex flex-col w-[200px] p-5 mt-10 space-y-3">
+          <Button
+            variant="destructive"
+            className="rounded-full"
+            onClick={() => handleClick()}
+            disabled={isPending}
+          >
+            {isPending ? <Loader2Icon className="animate-spin" /> : "Iniciar"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InitButtom;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
-const HomePage = () => {
-  return <h1>hello world</h1>;
+import { db } from "@/lib/prisma";
+import Image from "next/image";
+import { notFound, useParams } from "next/navigation";
+import InitButtom from "./[slug]/init/init-buttom";
+
+interface HomePageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const HomePage = async ({ params }: HomePageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findFirst({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="relative h-screen items-center justify-center px-4 pt-10">
+      {/*Logo do restaurante*/}
+      <div className="flex flex-col items-center gap-2 pt-44">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        {/*Nome do restaurante*/}
+        <h2 className="font-semibold">{restaurant?.name}</h2>
+        <div className="relative items-center justify-center gap-2 pt-10">
+          {/*Texto*/}
+          <p className="opacity-55 text-center mt-3">
+            Pelo aplicativo você poderá realizar seus pedidos para delivery ou
+            retirada de um jeito rápido e prático, e podendo concorrer a
+            sorteios e brindes especiais.
+          </p>
+        </div>
+        {/*Botão Iniciar*/}
+        <InitButtom />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
